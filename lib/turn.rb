5fd7f5a47b8f2e13189368cdf15f0c14719542a0
Justin K <justin-kim1@hotmail.com>
2018-01-14 02:45:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,6 +30,9 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player = "X")
+  else
+    
+  end
     display_board(board)
   end
 end
